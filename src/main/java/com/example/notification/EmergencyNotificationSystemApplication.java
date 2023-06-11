f5c2c57e149c64c1afb55cdc4cca6582c667120d
Chudnovskyi <chudnovskiy0@gmail.com>
2023-06-11 20:20:08 +0200

    first commit

---
+++ src/main/java/com/example/notification/EmergencyNotificationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyNotificationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmergencyNotificationSystemApplication.class, args);
+    }
+
+}
